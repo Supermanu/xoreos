283298e23cb30d3a8f53b0e266ccf6c786154a99
Supermanu <manueltondeur@gmail.com>
2013-11-23 16:01:59 +0100

    NWN: Fix the listbox widget.

--- src/engines/nwn/gui/widgets/listbox.h
+++ src/engines/nwn/gui/widgets/listbox.h
@@ -35,8 +35,6 @@
 #include "graphics/aurora/types.h"
 
 #include "engines/nwn/gui/widgets/modelwidget.h"
-#include "engines/nwn/gui/widgets/portrait.h"
-
 
 namespace Common {
 	class UString;
@@ -112,6 +110,7 @@ private:
 	float _fontHeight;
 	float _spacing;
 };
+
 
 /** A button widget within a NWN listbox widget with a text and an icon. */
 class WidgetListItemButton : public WidgetListItem {
@@ -191,11 +190,6 @@ public:
 
 	void mouseDown(uint8 state, float x, float y);
 	void mouseWheel(uint8 state, int x, int y);
-
-	/** Set the scrollbar position.
-	 * @param position The value must be in the interval [0,1]. 
-	 */
-	void setScrollBarPosition(float position);
 
 	void subActive(Widget &widget);
 
